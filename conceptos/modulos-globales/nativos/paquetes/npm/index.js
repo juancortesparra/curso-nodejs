54a9008390b9ec8bea502d69341f718e56da10fe
juan cortes <jcamilocortesbespoke@gmail.com>
2021-11-22 22:08:16 +0000

    adjunto ejercicios sobre numeros impar y par con paquetes de npm

---
+++ conceptos/modulos-globales/nativos/paquetes/npm/index.js
@@ -0,0 +1,4 @@
+const isOdd = require('is-odd'); // is-Odd es la dependencia para impares
+// is-Even es la dependencia de numeros pares
+console.log(isOdd(3)); // Es true por que es impar.
+console.log(isOdd(2)); // Es false por que par.
